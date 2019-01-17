5deea25cbe82937591bb754d9efe25dcbfd06ff5
Wojciech <s.wojciech9@gmail.com>
2019-01-17 09:27:22 +0100

    Huffman.asm improved

--- Semester_3/ARKO/x86/huffman.asm
+++ Semester_3/ARKO/x86/huffman.asm
@@ -58,22 +58,30 @@ extern createCodes
 
 ;do{
 huffman:
-
-;	int min = MAX, minest = MAX, minIndex = MAX, minestIndex = MAX;
     	mov ecx, DWORD[ebp+12]        	; ecx = &sign
-	mov edx, ecx
-	add edx, 8*256			; edx = finishSign
+	add ecx, 8*256			; edx = finishSign
 	push DWORD MAX			; EBP-4 = MIN
 	push DWORD MAX			; EBP-8 = MINEST
 	push DWORD MAX			; EBP-12 = minIndex
 	push DWORD MAX 			; EBP-16 = minestIndex
+	push DWORD edx			; EBP-20 = new_ind
+	add edx, 8*256	
+	push DWORD edx			; EBP-24 = finish_sign
+huffman_loop:
+;	int min = MAX, minest = MAX, minIndex = MAX, minestIndex = MAX;
+    	mov ecx, DWORD[ebp+12]        	;sign
+	mov edx, DWORD[ebp-24]		;finish_sign 
+	mov DWORD[ebp-4],  MAX
+	mov DWORD[ebp-8],  MAX
+	mov DWORD[ebp-12], MAX
+	mov DWORD[ebp-16], MAX
 
 ;	for(char sign: tab1){
 	sub ecx, 8
 lop:
 	add ecx, 8
 	cmp ecx, edx
-	je loop2_init
+	je finish 
 ;		if( tab[sign].parent == ROOT ){
 	mov ax, WORD[ecx+4]
 	cmp ax, ROOT
@@ -82,12 +90,12 @@ check_if_root:
 is_root:	;TODO delete
 ;			if( tab[sign].count < min ){
 	mov eax, DWORD[ecx]						; eax = tab[sign].count
-	mov ebx, DWORD[ebp-4]
+	mov ebx, DWORD[ebp-4];min
 	cmp eax, ebx
 	jae lop
 is_smaller:	;TODO delete
 ;				if( tab[sign].count < minest ){
-	mov ebx, DWORD[ebp-8]
+	mov ebx, DWORD[ebp-8];minest
 	cmp eax, ebx
 	jae change_min
 is_smallest:	;TODO delete
@@ -114,58 +122,6 @@ change_min:
 ;			}
 ;		}
 ;	}
-loop2_init:
-;	short sign;
-	add edx, 256*8
-	sub ecx, 8
-;	for(sign: tab2){
-;COPIED		if( ... == ROOT){ ... }
-loop2:
-	add ecx, 8
-	cmp ecx, edx
-	je finish
-;		if( tab[sign].count == 0 ){
-	mov ebx, DWORD[ecx]						; ebx = tab[sign].count
-	test ebx, ebx
-;			break;
-;		}
-	jz finish
-;		else if( tab[sign].parent == ROOT){
-	mov ax, WORD[ecx+4]
-	cmp ax, ROOT
-	jne loop2
-
-;			if( tab[sign].count < min ){
-	mov eax, ebx							; eax = tab[sign].count
-	mov ebx, DWORD[ebp-4]
-	cmp eax, ebx
-	jge loop2
-;				if( tab[sign].count < minest ){
-	mov ebx, DWORD[ebp-8]
-	cmp eax, ebx
-	jge change_min2
-;					min = minest;
-	mov ebx, DWORD[ebp-8]
-	mov DWORD[ebp-4], ebx
-;					minInd = minestInd;
-	mov ebx, DWORD[ebp-16]
-	mov DWORD[ebp-12], ebx
-;					minest = tab[sign].count;
-	mov DWORD[ebp-8], eax
-;					minestInd = sign;
-	mov DWORD[ebp-16], ecx
-;				}
-	jmp loop2
-;				else{
-change_min2:
-;					min = tab[sign].count;
-	mov DWORD[ebp-4], eax
-;					minInd = sign;
-	mov DWORD[ebp-12], ecx
-;				}
-	jmp loop2
-;			}
-;		}
 finish:
 ;	if(min == MAX){
 ;		break;//huffman ready
@@ -179,7 +135,10 @@ finish:
 				; EBP-12 = minIndex
 				; EBP-16 = minestIndex
 ;	short ind = sign;
-						; ecx = tab[ind]
+	mov ecx, DWORD[ebp-20]	; ecx = tab[ind]
+	mov ebx, ecx
+	add ebx, 8 
+	mov DWORD[ebp-20], ebx
 ;	tab[ind].count = min + minest;
 	;mov eax, DWORD[ebp-4]
 	mov ebx, DWORD[ebp-8]
@@ -193,20 +152,20 @@ finish:
 	mov ebx, DWORD[ebp+12]		; ebx = tab
 	sub ecx, ebx
 	shr ecx, 3
-	pop eax
+	mov eax, DWORD[ebp-16]	
 	mov WORD[eax+4], cx 
 ;	tab[minest].flag = 0;
 	mov BYTE[eax+6], 0
 ;	tab[min].parent = ind;
-	pop eax
+	mov eax, DWORD[ebp-12]
 	mov WORD[eax+4], cx 
 ;	tab[min].flag = 1;
 	mov BYTE[eax+6], 1
 ;}while(true)
-	pop eax
-	pop eax
-	jmp huffman
+	jmp huffman_loop
 epilog:
+	pop	eax	; ebp-24
+	pop	eax	; ebp-20
     	pop 	eax	; ebp-16
 	pop	eax	; ebp-12
 	pop	eax	; ebp-8
